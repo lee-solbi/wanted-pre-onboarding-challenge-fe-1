9abe7cb4004d9c90032f1fb03d6fb8a6efb23ca5
lee-solbi <solbi0221@gmail.com>
2022-08-07 21:46:04 +0900

    router 추가 및 title 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './containers/Login';
+import SignUp from './containers/SignUp';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/signUp' element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
